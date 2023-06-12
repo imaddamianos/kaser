91e11f738cd64df4287064c51704e800fc5daf6b
iMad <damianos.imad@gmail.com>
2023-06-12 16:31:41 +0300

    adding add store

---
+++ kaser/ViewControllers/Store/AddStoreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddStoreViewController.swift
+//  kaser
+//
+//  Created by iMad on 12/06/2023.
+//
+
+import UIKit
+
+class AddStoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- kaser/ViewControllers/sideMenu/MyStoreViewController.swift
+++ kaser/ViewControllers/sideMenu/MyStoreViewController.swift
@@ -6,9 +6,8 @@
 //
 
 import UIKit
-import Floaty
 
-class MyStoreViewController: UIViewController, FloatyDelegate {
+class MyStoreViewController: UIViewController{
 
     @IBOutlet weak var storeNameLbl: UILabel!
     @IBOutlet weak var storeNbLbl: UILabel!
@@ -19,59 +18,21 @@ class MyStoreViewController: UIViewController, FloatyDelegate {
     @IBOutlet weak var myProductsTbl: UITableView!
     @IBOutlet weak var coverImg: UIImageView!
 
-    var floaty: Floaty!
-
+    @IBOutlet weak var addStoreBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        configureFloaty()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        positionFloatyButton()
     }
-
+    @IBAction func addStoreTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "AddStoreNavID", sender: self)
+    }
+    
     func setupView() {
         sideMenuBtn.target = self.revealViewController()
         sideMenuBtn.action = #selector(self.revealViewController()?.revealSideMenu)
-    }
-
-    func configureFloaty() {
-        floaty = Floaty()
-        floaty.fabDelegate = self
-
-        floaty.addItem("createContact", icon: UIImage(named: "contact")) { item in
-            // Handle the "createContact" action
-            print("Create Contact tapped")
-        }
-
-        floaty.addItem("copyNumber", icon: UIImage(named: "email")) { item in
-            // Handle the "copyNumber" action
-            print("Copy Number tapped")
-        }
-
-        // Add more items as needed...
-
-        floaty.buttonColor = UIColor.systemBlue
-        floaty.plusColor = UIColor.white
-        floaty.overlayColor = UIColor.black.withAlphaComponent(0.3)
-
-        self.view.addSubview(floaty) // Add Floaty to the view hierarchy
-    }
-
-    func positionFloatyButton() {
-        let floatyButtonSize = floaty.frame.size
-        let xOffset = (view.frame.width - floatyButtonSize.width) / 2
-        let yOffset = view.frame.height - floatyButtonSize.height - 100
-
-        floaty.frame.origin = CGPoint(x: xOffset, y: yOffset)
-    }
-
-    // MARK: - FloatyDelegate
-
-    func clicked(on item: FloatyItem) {
-        // Handle item selection events here
-        print("Floaty item clicked: \(item.title)")
     }
 }
