b099ad2a9a5676d3560b4c2bfeb050cb0f226b94
iMad <damianos.imad@gmail.com>
2023-08-30 20:10:23 +0300

    fixing auto login and logos

---
+++ kaser/ViewControllers/LaunchScreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LaunchScreenViewController.swift
+//  kaser
+//
+//  Created by iMad on 30/08/2023.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController, LogInViewProtocol {
+    
+    var presenter: LogInPresenter!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presenter = LogInPresenter(view: self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let email = UserDefaults.standard.value(forKey: "email") as? String {
+            if let password = UserDefaults.standard.value(forKey: "password") as? String {
+                if (UserDefaults.standard.bool(forKey: "isSaveSelected") == true){
+                self.presenter.checkTxtField(email: email, password: password)
+            }
+        }
+        
+    }else{
+        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LogInVC") as UIViewController
+        viewController.modalPresentationStyle = .fullScreen
+        UIApplication.present(viewController: viewController)
+    }
+}
+
+}
